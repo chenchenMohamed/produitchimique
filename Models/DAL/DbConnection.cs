9ddbdbd2f882ff8419d5430e40c95581a2b5f1c7
Mohamed hedi Chenchen <chenchenmohamedhedi@gmail.com>
2021-03-11 22:48:38 +0100

    first commit

---
+++ Models/DAL/DbConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GestionProduitChimiques.Models.DAL
+{
+    public class DbConnection
+    {
+
+       // static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GPCDB";
+        static string connectionString = "Server=localhost;Database=mygpc;Uid=root;Pwd=1ngenieur.c0m;";
+
+        public static MySqlConnection GetConnection()
+        {
+            MySqlConnection cn = null;
+            try
+            {
+                cn = new MySqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+            return cn;
+
+        }
+    }
+}
